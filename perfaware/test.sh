fd7514137b3d371d673ca4eb2c258e4c9c59f0ee
Paul Kennedy <paul.kennedy124@gmail.com>
2023-03-19 21:53:23 -0500

    Using -O2 optimization. Using repeated listing for testing.

--- perfaware/test.sh
+++ perfaware/test.sh
@@ -2,20 +2,23 @@
 
 set -xe
 
-gcc -o sim8086.exe sim8086.c
+gcc -O2 -o sim8086.exe sim8086.c
 
 FILES=(
-    listing_0037_single_register_mov
-    listing_0038_many_register_mov
-    listing_0039_more_movs
-    listing_0040_challenge_movs
+#    listing_0037_single_register_mov
+#    listing_0038_many_register_mov
+#    listing_0039_more_movs
+#    listing_0040_challenge_movs
+    listing_0041_all_repeated
 )
 
 for INPUT in "${FILES[@]}"
 do
+    nasm $INPUT.asm
     ./sim8086.exe $INPUT > output.asm
     nasm output.asm
     diff -q output $INPUT
 done
 
 rm output.asm output
+echo "ALL TESTS PASSED"
