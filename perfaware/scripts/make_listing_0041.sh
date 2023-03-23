95863364141de0100c1fe0fc56e7192ebc01a3ca
Paul Kennedy <paul.kennedy124@gmail.com>
2023-03-22 22:09:19 -0500

    Moved some things around, deleted another unwanted file

--- perfaware/scripts/make_listing_0041.sh
+++ perfaware/scripts/make_listing_0041.sh
@@ -4,7 +4,7 @@ N="${1:-1}"
 
 rm -f input/listing_0041_all_repeated.asm
 for i in $(seq $N); do
-    echo "; ====" >> listing_0041_all_repeated.asm
+    echo "; ====" >> input/listing_0041_all_repeated.asm
     cat input/listing_0037_single_register_mov.asm  \
         input/listing_0038_many_register_mov.asm    \
         input/listing_0039_more_movs.asm            \
